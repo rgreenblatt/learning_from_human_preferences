a41c663b498d34cb31c0ef3d88bb4778fcc6482c
Neev Parikh <neev_parikh@brown.edu>
2021-10-15 01:04:35 -0400

    Removed branch

--- src/model.py
+++ src/model.py
@@ -22,3 +22,18 @@ def atari_policy_value_function_model(obs_n_channels, n_actions):
             nn.Linear(512, 1),
         ),
     )
+
+
+def reward_predictor_model(obs_n_channels):
+    return nn.Sequential(
+        nn.Conv2d(obs_n_channels, 32, 8, stride=4),
+        nn.ReLU(),
+        nn.Conv2d(32, 64, 4, stride=2),
+        nn.ReLU(),
+        nn.Conv2d(64, 64, 3, stride=1),
+        nn.ReLU(),
+        nn.Flatten(),
+        nn.Linear(3136, 64),
+        nn.ReLU(),
+        nn.Linear(64, 1),
+    )
