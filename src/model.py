7e2f357b67edf3416ffe9b3102bd8280ce183ad4
rgreenblatt <greenblattryan@gmail.com>
2021-10-16 21:37:53 -0400

    mess with model to change initial conditions

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 import pfrl
 from pfrl.policies import SoftmaxCategoricalHead
@@ -24,6 +25,20 @@ def atari_policy_value_function_model(obs_n_channels, n_actions):
     )
 
 
+class AddX(nn.Module):
+    def __init__(self, adder):
+        super().__init__()
+        self.register_buffer('adder', torch.tensor(float(adder)))
+
+    def forward(self, x):
+        return x + self.adder
+
+
+def modify_bias_init(layer: nn.Linear, bias: float) -> nn.Linear:
+    nn.init.constant_(layer.bias, bias)
+    return layer
+
+
 def reward_predictor_model(obs_n_channels):
     return nn.Sequential(
         nn.Conv2d(obs_n_channels, 32, 8, stride=4),
@@ -35,6 +50,7 @@ def reward_predictor_model(obs_n_channels):
         nn.Flatten(),
         nn.Linear(3136, 64),
         nn.ReLU(),
-        nn.Linear(64, 1),
-        nn.CELU()
+        modify_bias_init(nn.Linear(64, 1), -4.0),
+        nn.CELU(alpha=1.0),
+        AddX(1.0),
     )
