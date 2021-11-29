812d6ee72ade9317ef8e0e2239e5cb12b589a7f7
rgreenblatt <greenblattryan@gmail.com>
2021-11-28 20:03:17 -0500

    fix reward model training dataset

--- src/reward_model_training_dataset.py
+++ src/reward_model_training_dataset.py
@@ -10,28 +10,25 @@ class RewardModelTrainingDataset(Dataset):
     Dataset which allows for sliding window of states and preferences.
 
     We sample older trajectories exponentially less frequently. Precisely, a
-    group of trajectories added i iterations ago is sampled with probability
+    group of trajectories with time i ago is sampled with probability
     proportional to sample_decay**i.
 
     To save on memory, if the probability of sampling from a group of
     trajectories is less than min_sample_prob, that group is eliminated.
 
     The size is arbitrary because we always sample randomly, but
-    it is computed as round((size_multiplier * n / p) / batch_size) * batch_size
+    it is computed as ??? (TODO, maybe change this)
     Dividing and multiplying by batch_size ensures the size is a multiple
     of batch size. This is only done if batch size isn't none.
-    Where:
-        p: probability of sampling from most recent group
-        n: number of trajectories in most recent group
 
     Args:
         sample_decay (float): see above
         min_sample_prob (float): see above
-        epoch_size_multiplier (float): see above
+        epoch_size_multiplier (float): ??? (use this?)
     """
     def __init__(
         self,
-        sample_decay: float = 0.5,
+        sample_decay: float = 0.99999,
         min_sample_prob: float = 1e-4,
         size_multiplier: float = 1.5,
         batch_size: Optional[int] = None,
@@ -48,12 +45,19 @@ class RewardModelTrainingDataset(Dataset):
         self._size_multiplier = size_multiplier
         self._batch_size = batch_size
 
-        self._states : List[torch.Tensor] = []
-        self._mus : List[torch.Tensor] = []
-        self._times : List[Tuple[int, int]] = []
+        self._samples: List[Tuple[torch.Tensor, torch.Tensor, float]] = []
         self._probs: List[float] = []
 
-    def add_trajectories(self, states: List[torch.Tensor], mus: List[torch.Tensor], times : List[Tuple[int, int]]) -> None:
+    def _times_to_weight(self, l: int, r: int) -> float:
+        # could use func other than max...
+        return self._sample_decay**(-max(l, r))
+
+    def add_trajectories(
+        self,
+        states: List[torch.Tensor],
+        mus: List[torch.Tensor],
+        times: List[Tuple[int, int]]
+    ) -> None:
         """
         Args:
             states (List[Tensor]): List of size n, tensor: [2 x k x (state dim)]
@@ -70,39 +74,38 @@ class RewardModelTrainingDataset(Dataset):
         assert len(states) == len(mus)
         assert len(states) == len(times)
 
-        self._states += states
-        self._mus += mus
-        self._times += times
+        # not exactly efficient...
+        self._samples += [
+            (state, mu, self._times_to_weight(*tms)) for state,
+            mu,
+            tms in zip(states, mus, times)
+        ]
+        self._samples.sort(key=lambda x: x[2])
         self._compute_probs()
 
-        if self._probs[0] < self._min_sample_prob:
-            del self._samples[0]
-            self._compute_probs()
+        remaining = 1.
+        i = 0
 
-    def _raw_proportional_constant(self, time: int) -> float:
-        return self._sample_decay**(-time)
+        for i, prob in enumerate(self._probs):
+            if prob / remaining >= self._min_sample_prob:
+                break
+            remaining -= prob
+            assert remaining > 0.
+
+        del self._samples[:i]
+        self._compute_probs()
 
     def _compute_probs(self):
-        # could use func other than max...
-        norm_const = sum(
-            self._raw_proportional_constant(max(*tms))
-            for tms in self._times
-        )
-        self._probs = [
-            self._raw_proportional_constant(max(*tms)) / norm_const
-            for tms in self._times
-        ]
+        norm_const = sum(weight for _, _, weight in self._samples)
+        self._probs = [weight / norm_const for _, _, weight in self._samples]
 
     def current_num_labels(self) -> int:
-        return sum(states.size(0) for states, _ in self._samples)
+        return len(self._samples)
 
     def __len__(self) -> int:
         if len(self._samples) == 0:
             return 0
-        raw_size = (
-            self._size_multiplier * self._samples[-1][0].size(0) /
-            self._probs[-1]
-        )
+        raw_size = len(self._samples)  # TODO: this could be more efficient!
         if self._batch_size is not None:
             return max(round(raw_size / self._batch_size), 1) * self._batch_size
         else:
@@ -112,11 +115,9 @@ class RewardModelTrainingDataset(Dataset):
     def __getitem__(self, _) -> Tuple[torch.Tensor, torch.Tensor]:
         # TODO: consider instead doing this with numpy generator object and
         # accepting seed as arg
-        group_idx = np.random.choice(len(self._samples), p=self._probs)
-        n = self._samples[group_idx][0].size(0)
-        idx = np.random.randint(0, n)
+        idx = np.random.choice(len(self._samples), p=self._probs)
 
-        return self._samples[group_idx][0][idx], self._samples[group_idx][1][idx]
+        return self._samples[idx][0], self._samples[idx][1]
 
 
 if __name__ == "__main__":
@@ -131,13 +132,20 @@ if __name__ == "__main__":
         print(batched.shape)
         print(batched[0].size(), batched[1].size())
 
-    print("adding trajs tests")
-    for _ in range(10):
-        dataset.add_trajectories(
-            torch.rand(100, 2, 30), torch.full((100,), False)
-        )
-        print(dataset._probs)
-        print(len(dataset))
+    dataset.add_trajectories(
+        [torch.rand(2, 1, 1) for _ in range(10)],
+        [torch.full((2,), 0.5) for _ in range(10)],
+        [(i * 10000, i * 10000) for i in range(10)],
+    )
+    print(dataset._probs)
+    print(len(dataset))
+    dataset.add_trajectories(
+        [torch.rand(2, 1, 1) for _ in range(10, 100)],
+        [torch.full((2,), 0.5) for _ in range(10, 100)],
+        [(i * 10000, i * 10000) for i in range(10, 100)],
+    )
+    print(dataset._probs)
+    print(len(dataset))
 
     # sampling??
     for _ in range(100):
